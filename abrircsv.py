7228adbe2edcd7d67ab3f9a6800c39f340996e7b
franxxxx <161629154+moyanofran@users.noreply.github.com>
2025-04-07 13:40:21 -0300

    Update abrircsv.py

--- abrircsv.py
+++ abrircsv.py
@@ -2,4 +2,18 @@ import pandas as pd
 
 df=pd.read_csv("StudentsPerformance.csv")
 
-print(df)
+print(df.head()) #mostrar cabeza
+
+print(df.teil(20)) #mostrar pie
+
+print(df.to_string()) #muestra todo
+
+print(df.shape) #tira dos numeros, cantidadde filas y columnas
+
+print(df.dtypes) #mostrar ostipos de dato
+
+print(df.info()) #muestra datos sin completar ( no null), para limpiar
+
+print-(df.describe()) #da datos estadisticos de la tabla
+
+print(df[["gender","maxcore"]].head()) # muesra una columna
