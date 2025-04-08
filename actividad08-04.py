78b0bda15cef345a26665e951380464b7b1b87b9
franxxxx <161629154+moyanofran@users.noreply.github.com>
2025-04-08 13:03:52 -0300

    Add files via upload
    actividad 08/04

---
+++ actividad08-04.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x=[1,2,3,4,5] 
+
+y=[10,15,20,25,30]
+
+plt.plot(x,y) #creacion de grafico
+
+plt.xlabel("eje x: ")
+
+plt.ylabel("eje y: ") #etiquetas
+
+plt.title(" grafico de ejemplo ")
+
+plt.show() #mostrar grafico
