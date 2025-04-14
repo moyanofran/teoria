7157c48ce01f263bb2c248e066b599427d80eb75
franxxxx <161629154+moyanofran@users.noreply.github.com>
2025-04-14 14:14:10 -0300

    Update actividad_14-04.py

--- actividad_14-04.py
+++ actividad_14-04.py
@@ -14,3 +14,25 @@ a=np.arange(0,1000)
 df['columna de arreglo'] = a
 print=df
 
+#crear uan columna con numeros
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv('StudentsPerfonmance.csv')
+
+a=np.random.randit(1,100,size=1000)
+
+df['columna de arreglo'] = a
+
+print(df)
+# crea con numeros decimales
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv('StudentsPerfonmance.csv')
+
+a=np.random.uniform(1,100,size=1000)
+
+df['columna de arreglo'] = a
+
+print(df)
